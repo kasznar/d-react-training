5f223a275788cc9381f2520a6e4ad547c7f7631f
Kasznár Dávid <kasznardavid8@gmail.com>
2021-09-23 11:09:40 +0200

    require login when commenting

--- src/product/useComments.ts
+++ src/product/useComments.ts
@@ -1,15 +1,28 @@
-import { ChangeEvent, useCallback, useEffect, useState } from "react";
+import {
+  ChangeEvent,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { Comment, getComments, postComment } from "../api/comments";
+import { UserContext } from "../context/User";
 
 export const useComments = (productId: number) => {
   const [newComment, setNewComment] = useState("");
   const [comments, setComments] = useState<Comment[]>([]);
+  const { isLoggedIn, login } = useContext(UserContext);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setNewComment(event.target.value);
   };
 
   const handleSubmit = async () => {
+    if (!isLoggedIn) {
+      login();
+      return;
+    }
+
     if (newComment.trim()) {
       await postComment({ productId, text: newComment });
       setNewComment("");
