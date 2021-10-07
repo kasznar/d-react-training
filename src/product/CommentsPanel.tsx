97d206f3298410ddaaeb2b70d1af77a29a6fe170
Kasznár Dávid <kasznardavid8@gmail.com>
2021-10-07 14:28:56 +0200

    refresh after new comment

--- src/product/CommentsPanel.tsx
+++ src/product/CommentsPanel.tsx
@@ -1,25 +1,9 @@
-import React, {ChangeEvent, FC, useEffect, useState} from "react";
-import {Comment, getComments, postComments} from "../api/comments";
+import React, {FC} from "react";
 import {Button, Paper, TextField, Typography} from "@mui/material";
+import {useComments} from "./useComments";
 
 export const CommentsPanel: FC<{ productId: string }> = ({ productId }) => {
-  const [comments, setComments] = useState<Comment[]>([]);
-  const [newComment, setNewComment] = useState('');
-
-  useEffect(() => {
-    getComments(productId).then((data) => {
-      setComments(data);
-    });
-  }, [productId, setComments]);
-
-  function handleChange(event: ChangeEvent<HTMLInputElement>) {
-    setNewComment(event.target.value);
-  }
-
-  async function handleClick() {
-        await postComments({productId, text: newComment});
-      setNewComment('');
-  }
+  const {handleClick, comments, handleChange, newComment} = useComments(productId)
 
   return (
     <React.Fragment>
---
+++ src/product/useComments.ts
@@ -0,0 +1,28 @@
+import {ChangeEvent, useCallback, useEffect, useState} from "react";
+import {Comment, getComments, postComments} from "../api/comments";
+
+export const useComments = (productId: string) => {
+    const [comments, setComments] = useState<Comment[]>([]);
+    const [newComment, setNewComment] = useState('');
+
+    const fetchComments = useCallback(async ()=>{
+        const res = await getComments(productId);
+        setComments(res);
+    }, [productId, setComments])
+
+    useEffect(() => {
+        fetchComments()
+    }, [fetchComments]);
+
+    function handleChange(event: ChangeEvent<HTMLInputElement>) {
+        setNewComment(event.target.value);
+    }
+
+    async function handleClick() {
+        await postComments({productId, text: newComment});
+        setNewComment('');
+        await fetchComments();
+    }
+
+    return {comments, handleChange, handleClick, newComment}
+}
