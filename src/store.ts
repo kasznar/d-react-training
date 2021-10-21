c776736c14ea49c905e8bef76516d1ed0a499a65
Kasznár Dávid <kasznardavid8@gmail.com>
2021-10-21 15:12:18 +0200

    redux setup

---
+++ src/store.ts
@@ -0,0 +1,25 @@
+import {AnyAction, applyMiddleware, createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk, {ThunkAction} from "redux-thunk";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import {rootReducer} from "./features";
+
+const enhancers = composeWithDevTools(applyMiddleware(thunk))
+
+
+const store = createStore(rootReducer, enhancers);
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    AnyAction
+    >;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
